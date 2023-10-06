ec5fb505ed04bf2a441d18a870107ee6b644a229
Joshua <fatow.jay@gmail.com>
2023-10-06 18:15:38 +0000

    a program that prints the last digit of a randomly assigned number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is ", n);
+	if (n > 5)
+	{
+		printf("and is greater than 5");
+	} else if (n == 0)
+	{
+		printf("and is 0");
+	} else
+	{
+		printf("and is less than 6 and not 0");
+	}
+	return (0);
+}
