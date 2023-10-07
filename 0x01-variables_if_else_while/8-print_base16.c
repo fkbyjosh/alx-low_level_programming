17bbef2e928e017309fcbe360b56000297856004
Joshua <fatow.jay@gmail.com>
2023-10-07 13:16:52 +0000

    update to 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,10 +11,10 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i <= 57; i++)
 		putchar(i);
-	for (j = 97; j < 103; j++)
-		putchar(i);
+	for (j = 97; j <= 102; j++)
+		putchar(j);
 	putchar('\n');
 
 	return (0);
