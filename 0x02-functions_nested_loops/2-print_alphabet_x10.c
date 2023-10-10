42675d632b67ce4bf6d662d33426bc0149dc08b6
Joshua <fatow.jay@gmail.com>
2023-10-10 21:40:12 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print ten alphabets
+ *
+ *Return: it returns no value
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char alph;
+
+	while (i <= 10)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+		i++;
+	}
+}
