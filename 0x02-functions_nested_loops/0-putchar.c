862f24f926713337b50fb4d26dd7e2d9954556ba
Joshua <fatow.jay@gmail.com>
2023-10-10 20:48:05 +0000

    update to 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,14 @@
 
 int main(void)
 {
-	char c[] = "_putchar\n";
-	int i;
-
-	for (i = 0; c[i] != '\0'; i++)
-	{
-		_putchar(c[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
