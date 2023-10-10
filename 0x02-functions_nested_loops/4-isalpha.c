c587152631a21bf01947af69d324bce03fff6723
Joshua <fatow.jay@gmail.com>
2023-10-10 22:28:21 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if c is an alphabet
+ * @c: character to be checked
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
