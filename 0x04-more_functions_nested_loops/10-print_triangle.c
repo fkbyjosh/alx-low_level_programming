7e9b371cdcc3847e16a104df2a28b86f4b650825
Joshua <fatow.jay@gmail.com>
2023-10-13 03:15:37 +0000

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ * Return: returns no value
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			printf(" ");
+		}
+		for (k = 0; k < i; k++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ * Return: returns no value
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int k;
+
+	for (i = 0; i < size; i++)
+	{
+		for(j = 0; j < size - i; j++)
+		{
+			printf(" ");
+		}
+		for (k = 0; k < i; k++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+}
