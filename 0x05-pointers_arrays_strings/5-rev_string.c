f55953ca0b4160d84722d0fc5b04fecff9099bcc
Joshua <fatow.jay@gmail.com>
2023-10-17 00:12:01 +0000

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char *end = s;
+
+	while (*end != '\0')
+	{
+		end++;
+	}
+
+	end--;
+
+	while (end >= s)
+	{
+		_putchar(*end);
+		end--;
+	}
+	_putchar('\n');
+}
