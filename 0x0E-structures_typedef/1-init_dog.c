a462c0bd3a0a27edc7a5629995d8d11a5cf50235
Joshua <fatow.jay@gmail.com>
2023-11-07 08:18:02 +0000

    new header file for 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include "stdlib.h"
+#include "stddef.h"
 
 /**
  * init_dog - Initializes a struct dog.
@@ -13,9 +14,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
