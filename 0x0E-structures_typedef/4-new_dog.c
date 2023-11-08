3356ea58e9c7471a0bce2bf72f7c7c756b645048
Joshua <fatow.jay@gmail.com>
2023-11-08 01:20:53 +0000

    4-new_dog.c still not working

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
-#include "dog.h"
 #include <stdlib.h>
 #include <string.h>
+#include "dog.h"
 
 /**
  * new_dog - creates a new dog
@@ -13,23 +13,42 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newdog = malloc(sizeof(dog_t));
+	dog_t *newdog;
+	int name_length = 0, owner_length = 0;
+	int i;
+
+	while (name[name_length] != '\0')
+		name_length++;
+	while (owner[owner_length] != '\0')
+		owner_length++;
+
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
-	new_dog->name = strdup(name);
-	new_dog->owner = strdup(owner);
-
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	new_dog->name = malloc(sizeof(char) * (name_length + 1));
+	if (new_dog->name == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
+
+	new_dog->owner = malloc(sizeof(char) * (owner_length + 1));
+
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+
+	for (i = 0; i <= name_length; i++)
+		new_dog->name[i] = name[i];
+	for (i = 0; i <= owner_length; i++)
+		new_dog->owner[i] = owner[i];
 
 	new_dog->age = age;
 
