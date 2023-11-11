ce461676651dd8a255c06534db2ce251cf34405f
Joshua <fatow.jay@gmail.com>
2023-11-11 14:55:00 +0000

    fixes to 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,6 +3,33 @@
 #include "function_pointers.h"
 
 /**
+ * main - entry point of program
+ * @argc: number of command line arguments
+ * @argv: argument vector
+ *
+ * Return: 0 (success), 1 (incorrect), 2 (negative)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	print_opcodes(num_bytes);
+	return (0);
+}
+
+/**
  * print_opcodes - prints the opcodes of its own main function
  * @num_bytes: number of bytes
  *
