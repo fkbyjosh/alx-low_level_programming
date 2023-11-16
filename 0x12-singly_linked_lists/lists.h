5e00fdf9be744a09b24d1a419558333f5de13d09
fkbyjosh <fatow.jay@gmail.com>
2023-11-16 23:32:17 +0000

    prototype correction

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,7 +20,7 @@ typedef struct list_s
 } list_t;
 
 /* Function prototypes */
-void pre_main(void);
+void __attribute__ ((constructor)) pre_main(void)
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
