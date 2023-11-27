e998580b9f3cee325b32cfaa6eb8373292dc5cff
fkbyjosh <fatow.jay@gmail.com>
2023-11-27 22:04:31 +0000

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and prints it
+ * to the POSIX standard output
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ * Return: Number of letters it could read and print, or 0 on failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+		return (0);
+
+	int file_descriptor = open(filename, O_RDONLY);
+
+	if (file_descriptor == -1)
+		return (0);
+	char buffer[1024];
+
+	ssize_t bytes_read, bytes_written, total_written = 0;
+
+	while ((bytes_read = read(file_descriptor, buffer, sizeof(buffer))) > 0)
+	{
+		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+		if (bytes_written == -1 || (size_t)bytes_written != (size_t)bytes_read)
+		{
+			close(file_descriptor);
+			return (0);
+		}
+
+		total_written += bytes_written;
+		if (total_written >= (ssize_t)letters)
+			break;
+	}
+
+	close(file_descriptor);
+	return (total_written);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,15 @@
+0. Tread lightly, she is near
+a function that reads a text file and prints it to the POSIX standard output.
+File: 0-read_textfile.c
+
+1. Under the snow
+a function that creates a file.
+File: 1-create_file.c
+
+2. Speak gently, she can hear
+a function that appends text at the end of a file.
+File: 2-append_text_to_file.c
+
+3. cp
+a program that copies the content of a file to another file.
+File: 3-cp.c
