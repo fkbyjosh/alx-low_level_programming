f72ce96f78dbf3e5e94c7566c472529e30234a4c
fkbyjosh <fatow.jay@gmail.com>
2023-11-27 22:51:11 +0000

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * error_exit - Print error message and exit with given code
+ * @code: Error code to exit with
+ * @message: Error message to print
+ */
+
+void error_exit(int code, char *message)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(code);
+}
+
+/**
+ * main - Entry point for copying content from one file to another
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of pointers to the arguments
+ * Return: 0 on success, various exit codes on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to");
+
+	file_from = open(argv[1], O_RDONLY);
+
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(file_to, buffer, bytes_read);
+
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(file_from) == -1 || close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		exit(100);
+	}
+	return (0);
+}
