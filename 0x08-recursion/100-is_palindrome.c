4a58a6620d2a0a9083cef274057543ef10b24957
Joshua Baka <fatow.jay@gmail.com>
2024-01-29 18:58:36 +0000

    0x18-dynamic_libraries

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to be checked
- * Return: 1 if a palindrome, 0 if not
- */
-
-int is_palindrome(char *s)
-{
-	int length = _strlen_recursion(s);
-
-	if (length <= 1)
-	{
-		return (1);
-	}
-	if (*s != s[length - 1])
-	{
-		return (0);
-	}
-	return (is_palindrome(s + 1));
-}
-
-/**
  * _strlen_recursion - returns the lengths of a string
  * @s: the string
  * Return: length of the string
@@ -34,4 +13,32 @@ int _strlen_recursion(char *s)
 		return (0);
 	}
 	return (1 + (_strlen_recursion(s + 1)));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to be checked
+ * Return: 1 if a palindrome, 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int length;
+
+	if (*s == '\0')
+	{
+		return (1);
+	}
+
+	length = _strlen_recursion(s);
+
+	if (length <= 1)
+	{
+		return (1);
+	}
+	if (*s != s[length - 1])
+	{
+		return (0);
+	}
+	return (is_palindrome(s + 1));
 }
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,13 @@
+0x18. C - Dynamic libraries
+
+0. A library is not a luxury but one of the necessities of life
+File: libdynamic.so, main.h
+
+1. Without libraries what have we? We have no past and no future
+File: 1-create_dynamic_lib.sh
+
+2. Let's call C functions from Python
+File: 100-operations.so
+
+3. Code injection: Win the Giga Millions!
+File: 101-make_me_win.sh
